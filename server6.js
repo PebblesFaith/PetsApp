841ba41966c214f3a432bf77dac1bf5763dbd47b
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-09-22 16:25:57 -0500

    Create server6.js
    Today (September 22, 2019), I had completed studying my online tutorial 9, 10, and 11. And, I had added additional comprehension into the mapping my static page textboxes to JQuery AJAX and SQL in creating a databases, inserting records, queries selections and deleting records from the database.
    
    You all have a great day!

---
+++ server6.js
@@ -0,0 +1,39 @@
+// Import the Express Library.
+const express = require('express');//
+const app = express();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('Pets.db');
+
+app.use(express.static('static_files'));
+
+const port = 3000;
+
+app.listen(port, function() {
+    console.log('Server is listening on start at http://locathost: ' + 3000 + '/');
+});
+
+
+    
+
+app.get('/users', function(request, response) {
+    db.all('SELECT name FROM users_to_pets', function (err, rows) {
+
+        console.log(rows);
+        const allUserNames = rows.map(e => e.name);
+        console.log(allUserNames);
+        response.send(allUserNames); 
+    
+    });    
+});
+
+// POST request is for posting new data to the server.
+const bodyParser = require('body-parser');
+// Connect to my web application.
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/users', function(request, response) {
+    console.log(request.body);
+    response.send({});
+});
+
+   
