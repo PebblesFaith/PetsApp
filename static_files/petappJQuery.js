0d3580a3129faeb05d2e081838b2dfbccdbe3c83
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-09-21 20:12:52 -0500

    Update petappJQuery.js
    Today (September 21, 2019), I had practiced JQuery AJAX and started on SQLite3 for lesson 6, 7, 8, and partial of 9.

--- static_files/petappJQuery.js
+++ static_files/petappJQuery.js
@@ -1,4 +1,53 @@
 $(document).ready(function() {
     console.log('Hello Browser!!!');
 
+    $('#readButton').click(function() {
+        const requestURL = 'users/' + $('#nameBox').val();
+        console.log('making ajax request to : ', requestURL);
+
+        $.ajax({
+            url: requestURL,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                console.log('You have received some data!', data);
+
+                // Apply Error Case if statement analysis.
+                if (data.job && data.pet) {
+                    $('#status').html('Successfully fetched data at URL:' + requestURL);
+                    $('#jobDiv').html('My title is ' + data.job);
+                    $('#petImage').attr('src', data.pet).attr('width', '300px');
+                }
+                else {
+                    $('#status').html('ERROR: Request could not find data URL: ' + requestURL);
+                    $('#jobDiv').html('');
+                    $('#petImage').attr('src', '').attr('width', '0px');
+                }
+            }
+        });
+
+    });
+
+
+    $('#allUsersButton').click(function() {       
+
+        $.ajax({
+            url: 'users/',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                console.log('You received some data!', data);
+                $('#status').html('All users: ' + data);
+            }
+        });        
+    });
+
+    // What does an AJAX Error Handler means, well, whenever there is an error in making an AJAX call function, as above, if any of these above
+    // AJAX functions does not handle correctly then the AJAX function below is a catch-all function for the above
+    // AJAX functions. The AJAX Error Handler below says, if something is wrong with the above click handler functions.
+    $(document).ajaxError(function() {
+        $('#status').html('ERROR: Unknown AJAX ERROR!');
+
+    });
+
 });
